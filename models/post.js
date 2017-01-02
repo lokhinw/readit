04d31dd74e446e38a85d66c27ee0e17865f866da
Lokhin Wu <lokhin.wu@live.com>
2017-01-01 22:01:22 -0500

    Added mongoose schema models

---
+++ models/post.js
@@ -0,0 +1,12 @@
+var mongoose = require("mongoose");
+
+var postSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment"
+    }]
+});
+
+module.exports = mongoose.model("Post", postSchema);
